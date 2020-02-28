1f9c7d94a10748718863be6bae1387440c7c83db
erison eduard <erison.eduard@fpf.br>
2020-02-28 16:17:46 -0400

    adicionando about/header/home/restaurantes-principais

---
+++ src/app/restaurants/retaurant/retaurant.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mt-retaurant',
+  templateUrl: './retaurant.component.html',
+  styleUrls: ['./retaurant.component.css']
+})
+export class RetaurantComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
